e4f3803c6efe06569f4c1c09ee47e806f718cd2c
Hoohm <patrick@NGS.Workstation>
2016-08-02 12:04:48 +0200

    first commit

---
+++ dropseq.py
@@ -0,0 +1,30 @@
+import subprocess
+import sys
+import os
+
+try:
+	sys.argv[1]
+except:
+	print('You need to provid a folder on which to run on. Exiting')
+	exit()
+wrkdir = sys.argv[1]
+configfile = os.path.join(wrkdir, 'config.json')
+FOLDERS = ['summary', 'logs','plots']
+
+if (not os.path.isfile(configfile)):
+	print('No configfile. Exiting')
+	exit()
+for folder in FOLDERS:
+	joined = os.path.join(wrkdir, folder)
+	if(not os.path.isdir(joined)):
+		os.mkdir(joined)
+#Optimized Run for dropseq
+first = 'snakemake -s snakefiles/Dropseq_pre_align.snake --cores 8 -pT -d {} --configfile local.json'.format(sys.argv[1])
+second = 'snakemake -s snakefiles/Star_align.snake --cores 8 -pT -d {} --configfile local.json'.format(sys.argv[1])
+third = 'snakemake -s snakefiles/Dropseq_post_align.snake --cores 8 -pT -d {} --configfile local.json'.format(sys.argv[1])
+print('Running pre_processing')
+subprocess.call(first, shell=True)
+print('Running Alignement')
+subprocess.call(second, shell=True)
+print('Running post_processing')
+subprocess.call(third, shell=True)
