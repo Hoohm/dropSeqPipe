4ff42906aa51032d034ba8d26768111c61c71f5d
hoohm <patrick.roelli@gmail.com>
2017-09-18 20:52:32 +0200

    typo on samples_yaml

--- dropSeqPipe/__main__.py
+++ dropSeqPipe/__main__.py
@@ -93,7 +93,7 @@ def main():
             joined = os.path.join(args.folder_path, folder)
             if(not os.path.isdir(joined)):
                 os.mkdir(joined)
-    if(sample_yaml['GLOBAL']['data_type'] not in ['singleCell', 'bulk']):
+    if(samples_yaml['GLOBAL']['data_type'] not in ['singleCell', 'bulk']):
         exit('Data_type value not valid. Please use singleCell or bulk.\nData_type is case sensitive!')
     # Select step and run
     # First step is fastqc
