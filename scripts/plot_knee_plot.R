62e01344684ac7a08fdfe5e42f13c9a9f007bbef
hoohm <patrick.roelli@gmail.com>
2018-01-21 01:26:43 +0100

    fixed knee_plot_whitelist rule

--- scripts/plot_knee_plot.R
+++ scripts/plot_knee_plot.R
@@ -19,7 +19,8 @@ knee_plot = knee_plot + labs(x='STAMPS', y='Cumulative fraction of reads')
 if(!is.null(snakemake@input$barcodes))
 {
 	barcodes_whitelist = read.csv(snakemake@input$barcodes, header=FALSE, stringsAsFactors=FALSE)
-	knee_plot = knee_plot + geom_point(data = plot_data[plot_data$Barcode %in% barcodes_whitelist$V1,], aes(x=rank, y=cum_sum, color='Whitelisted'), size=0.1) + scale_color_manual(values=c('Whitelisted'='green'))
+	knee_plot = knee_plot + geom_point(data = plot_data[plot_data$Barcode %in% barcodes_whitelist$V1,], aes(x=rank, y=cum_sum, color='Whitelisted'), size=0.1)
+	knee_plot = knee_plot + scale_color_manual(values=c('Whitelisted'='green'))
  }
-ggsave(file=snakemake@output$pdf, width = 4, height = 3)
-ggsave(file=snakemake@output$png, width = 4, height = 3)
+ggsave(knee_plot, file=snakemake@output$pdf, width = 4, height = 3)
+ggsave(knee_plot, file=snakemake@output$png, width = 4, height = 3)
