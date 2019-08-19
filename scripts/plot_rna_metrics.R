96fa05315313ad25a8fba89f2806b2af7e58577f
Sebastian Mueller <sebm@posteo.de>
2019-08-19 16:20:00 +0100

    fixed bug reading in cells from snakmake params; changed order of plot to original

--- scripts/plot_rna_metrics.R
+++ scripts/plot_rna_metrics.R
@@ -18,33 +18,44 @@ if (!is.null(snakemake@config$DEBUG)) {
 }
 #### /debug
 
-data <- read.csv(file = snakemake@input$rna_metrics, header = T, stringsAsFactors = F, skip = 6, sep = "\t")
-data <- data[order(data$PF_ALIGNED_BASES, decreasing = T), ]
-data_pct <- data[, c("READ_GROUP", "PCT_RIBOSOMAL_BASES", "PCT_CODING_BASES", "PCT_UTR_BASES", "PCT_INTRONIC_BASES", "PCT_INTERGENIC_BASES")]
-data <- data[, c("READ_GROUP", "RIBOSOMAL_BASES", "CODING_BASES", "UTR_BASES", "INTRONIC_BASES", "INTERGENIC_BASES")]
-data_long <- data %>% gather(read_overlap, count, -READ_GROUP)
-# Keep the order of the barcodes using factor and levels.
-my_sequences <- factor(unique(data_long$READ_GROUP))
-data_long$READ_GROUP <- factor(data_long$READ_GROUP, levels = my_sequences)
-p1 <- ggplot(data_long, aes(x = READ_GROUP, y = count, fill = read_overlap)) +
+mydata <- read.csv(file = snakemake@input$rna_metrics, header = T,
+                   stringsAsFactors = F, skip = 6, sep = "\t")
+mydata <- mydata[order(mydata$PF_ALIGNED_BASES, decreasing = T), ]
+mydata_pct <- mydata[, c("READ_GROUP", "PCT_RIBOSOMAL_BASES",
+                         "PCT_CODING_BASES", "PCT_UTR_BASES",
+                         "PCT_INTRONIC_BASES", "PCT_INTERGENIC_BASES")]
+mydata <- mydata[, c("READ_GROUP", "RIBOSOMAL_BASES", "CODING_BASES",
+                     "UTR_BASES", "INTRONIC_BASES", "INTERGENIC_BASES")]
+
+# converting into long format for ploting
+mydata_long <- mydata %>% gather(read_overlap, count, -READ_GROUP)
+
+# Keep the original order of the barcodes using factor and levels.
+mydata_long$READ_GROUP <- factor(mydata_long$READ_GROUP,
+                                 levels = factor(unique(mydata_long$READ_GROUP)))
+mydata_long$read_overlap <- factor(mydata_long$read_overlap,
+                                   levels = unique(mydata_long$read_overlap))
+
+p1 <- ggplot(mydata_long, aes(x = READ_GROUP, y = count, fill = read_overlap)) +
   geom_bar(stat = "identity") +
   theme(axis.text.x = element_text(angle = 90, hjust = 0))
-if (!is.null(snakemake@params$cells)) {
-  p1 <- p1 + labs(title = paste("Top", snakemake@params$cells, "barcodes for", snakemake@wildcards$sample),
+p1 <- p1 + labs(title = paste(nrow(mydata),
+                              "selected barcodes for",
+                              snakemake@wildcards$sample),
                   x = "Barcodes", y = "Bases")
-} else {
-  p1 <- p1 + labs(title = paste("Whitelisted barcodes for", snakemake@wildcards$sample),
-                  x = "Barcodes", y = "Bases")
-}
 p1 <- p1 + theme(axis.title.x = element_blank(),
                  axis.text.x = element_blank(),
                  axis.ticks.x = element_blank())
 p1 <- p1 + scale_y_continuous(labels = scales::scientific)
 
-data_long_pct <- data_pct %>% gather(read_overlap, fraction, -READ_GROUP)
-data_long_pct$READ_GROUP <- factor(data_long_pct$READ_GROUP,
-                                   levels = my_sequences)
-p2 <- ggplot(data_long_pct, aes(x = READ_GROUP, y = fraction, fill = read_overlap)) +
+mydata_long_pct <- mydata_pct %>% gather(read_overlap, fraction, -READ_GROUP)
+# Keep the original order of the barcodes using factor and levels.
+mydata_long_pct$READ_GROUP <- factor(mydata_long_pct$READ_GROUP,
+                                 levels = factor(unique(mydata_long_pct$READ_GROUP)))
+mydata_long_pct$read_overlap <- factor(mydata_long_pct$read_overlap,
+                                   levels = unique(mydata_long_pct$read_overlap))
+
+p2 <- ggplot(mydata_long_pct, aes(x = READ_GROUP, y = fraction, fill = read_overlap)) +
   geom_bar(stat = "identity") +
   theme(axis.text.x = element_text(angle = 90, hjust = 0)) +
   labs(x = "Barcodes", y = "%Bases") +
