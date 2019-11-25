2e95813844b3ce8bfd2b20364c82fd423284b140
TomKellyGenetics <tomkellygenetics@gmail.com>
2019-11-25 17:19:06 +0900

    add snakemake as miscellaneous in Seurat version 3

--- scripts/plot_violine.R
+++ scripts/plot_violine.R
@@ -219,7 +219,7 @@ ggsave(gg, file = snakemake@output$pdf_count_vs_gene, width = 12, height = 7)
 # sample1_CAGCCCTCAGTA   264  437 sample1_CAGCCCTCAGTA sample1 CAGCCCTCAGTA            100         100 batch1 sample1 0.0389016 0.07551487 0.1144165 0.0228833 0.5102975     1.655303
 
 # saving snakemake meta information into misc slot so all can be exported as one object
-seuratobj@misc <- snakemake
+Misc(object = pbmc_small, slot = "misc")  <- snakemake
 # exporting R Seurat objects into summary/R_Seurat_objects.rdata
 saveRDS(seuratobj, file = file.path(snakemake@output$R_objects))
 
